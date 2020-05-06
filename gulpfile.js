6696712208484ce161a507d1c0ea56e529fc2f71
Branyer <branyervergarac@gmail.com>
2020-05-05 23:57:52 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
+const watch = require('gulp-watch');
+
+gulp.task('less', () =>{
+    return gulp.src('less/styles.less')
+        .pipe(autoprefixer ({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(less({
+            includePath: ['less']
+        }))
+        .pipe(gulp.dest('css/styles.css'));
+});
+
+
+gulp.task('watch', gulp.series('less', ()=>{
+    watch('less/styles.less', gulp.registry().get('less'));
+}));
